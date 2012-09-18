f872560c4d135e46dd5e032c8953a8ffb9d99f16
Niall Cardin <NiallC@gmail.com>
2012-09-18 22:03:22 +0100

    fixed up some settings nonsense

--- mtgpuzzlerP/settingsLocal.py
+++ mtgpuzzlerP/settingsLocal.py
@@ -70,7 +70,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '',
+    
 )
 
 # List of finder classes that know how to find static files in
@@ -125,6 +125,8 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'mtgpuzzlerP',
+    'concepts',
 )
 
 # A sample logging configuration. The only tangible logging
