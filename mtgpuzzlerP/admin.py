ba90b3978685187f52ed7eb75004abe0059c95dd
Greg Pallis <mostlyconnect@gmail.com>
2012-09-18 22:41:56 +0100

    Improved admin view

--- mtgpuzzlerP/admin.py
+++ mtgpuzzlerP/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 from concepts.models import Card,Player,BitOfCardboard
 
+#admin.site.register(BitOfCardboard)
+
+class BitOfCardboardInline(admin.TabularInline):
+    model = BitOfCardboard
+    extra = 1
+    
+#class CardAdmin(admin.ModelAdmin):
+#    inlines = (BitOfCardboardInline,)
+
+class PlayerAdmin(admin.ModelAdmin):
+    inlines = (BitOfCardboardInline,)
+
 admin.site.register(Card)
-admin.site.register(Player)
-admin.site.register(BitOfCardboard)
+admin.site.register(Player, PlayerAdmin)
