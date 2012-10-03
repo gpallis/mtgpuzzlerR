420f4e6f5b660583c6847751d5d3b32b291e534a
gpallis <mostlyconnect@gmail.com>
2012-10-03 23:43:03 +0100

    Greg is the best at css in this actual room

--- puzzleViewer/views.py
+++ puzzleViewer/views.py
@@ -6,5 +6,9 @@ from django.shortcuts import render
 from concepts.models import Player
 
 def displayBattlefield(request):
-  return render(request, "displayBattlefield.html", {'players':Player.objects.all()})
+  player = Player.objects.get(name="Dave")
+  return render(request, "displayBattlefield.html", {
+    'player':player,
+    'handWidth':player.permanents.count()*175 + 10,
+  })
   #return HttpResponse("die in a fire")
